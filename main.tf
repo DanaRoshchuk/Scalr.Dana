36f5b2a1fad17b7189614865b2d860862767ae7d
DanaRoshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-03-02 14:53:26 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "run_id" {
+  default = "run_id_2"
+  #sensitive = true
+}
+variable "sleep_time" {
+  default = 3
+}
+variable "dynatrace-operator_enabled" {
+  description = "dynatrace-operator_enabled" 
+sensitive = true
+type = bool 
+  default = true
+  
+}
+resource "random_integer" "ran" {
+  count = 1
+  min = 31
+  max = 180
+  keepers = {
+    run_id = var.run_id
+  }
+}
