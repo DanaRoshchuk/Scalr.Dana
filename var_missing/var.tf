9bca08211fdad4fa3eac826c83cd845dbe59f442
DanaRoshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-05-10 13:06:41 +0300

    Update var.tf

--- var_missing/var.tf
+++ var_missing/var.tf
@@ -5,6 +5,10 @@ variable "prefix" {
 	default = ["Mr", "Mrs", "Sir"]
 	type = list
 }
+variable "run_id" {
+  default = "run_id_2"
+  #sensitive = true
+}
 variable "strings" { 
 default = ["a","b","c","d"]
 type = list(string)
