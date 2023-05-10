29ec4f4b518ec855d3a313565c58096358d7d198
DanaRoshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-05-10 13:54:38 +0300

    Update variables.tf

--- var_auto/variables.tf
+++ var_auto/variables.tf
@@ -1,16 +1,12 @@
 variable "quantity" {
-	default = 1
 	type = string
 }
 variable "prefix" {
-	default = ["Mr", "Mrs", "Sir"]
 	type = list
 }
 variable "run_id" {
-  default = "run_id_2"
   #sensitive = true
 }
 variable "strings" { 
-default = ["a","b","c","d"]
 type = list(string)
 }
