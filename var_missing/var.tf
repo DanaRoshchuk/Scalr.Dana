3065144ab6f8de0d73ba8c68d0e2b7dc29ce6f15
DanaRoshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-05-10 12:58:54 +0300

    Update var.tf

--- var_missing/var.tf
+++ var_missing/var.tf
@@ -2,10 +2,8 @@ variable "quantity" {
 	type = string
 }
 variable "prefix" {
+	default = ["Mr", "Mrs", "Sir"]
 	type = list
-}
-variable "run_id" {
-	 default = "run_id_2"
 }
 variable "strings" { 
 default = ["a","b","c","d"]
